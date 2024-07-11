4fe7228a2928dba3f2db6001a82e10bd15812d7f
Shivank33 <shivankchaudhary33@gmail.com>
2024-07-11 09:54:04 +0530

    issue fix my-app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -50,18 +50,18 @@ function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? '#13466e' : 'white',
                     color : props.mode === 'dark' ? 'white' : '#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-success mx-2" onClick={handleDownClick}>Convert to LowerCase</button>
-            <button className="btn btn-warning mx-2" onClick={countVowels}>Count Vowels</button>
-            <button className="btn btn-info mx-2" onClick={copyText}>Copy Text</button>
-            <button className="btn btn-dark mx-2" onClick={removeSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button disabled={text.length===0} className="btn btn-success mx-1 my-1" onClick={handleDownClick}>Convert to LowerCase</button>
+            <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={countVowels}>Count Vowels</button>
+            <button disabled={text.length===0} className="btn btn-info mx-1 my-1" onClick={copyText}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-dark mx-1 my-1" onClick={removeSpaces}>Remove Extra Spaces</button>
             {/* <button className="btn btn-warning mx-2" onClick={inverseText}>Inverse Text</button> */}
 
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
             <h2>Text Summary</h2>
-            <p>{text.split(/\s+/).filter((word) => word !== "").length} words and {text.length} characters</p>
-            <p>{text.split(/\s+/).filter((word) => word !== "").length * 0.008} minutes to read</p>
+            <p>{text.split(" ").filter((word) => word !== "").length} words and {text.length} characters</p>
+            <p>{text.split(" ").filter((word) => word !== "").length * 0.008} minutes to read</p>
             {/* 0.008 minutes to read one word. */}
             <h2>Preview</h2>
             <p>{text.length > 0 ? text : "Enter your text in textbox to preview here"}</p>
